4797fd48c9c0923cd37d65a294afef2e4c10c320
ankitbhanja79 <38048930+ankitbhanja79@users.noreply.github.com>
2019-01-26 02:06:22 +0530

    guvi-b2.cpp

---
+++ guvi-b2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	if(n%2==0)
+	{
+		cout<<"Even";
+	}
+	else if(n%2!=0)
+	{
+		cout<<"Odd";
+	}
+	else
+	{
+		cout<<"Invalid";
+	}
+	return 0;
+}
