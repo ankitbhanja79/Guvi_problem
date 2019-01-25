2b0cda530a49ddc3b7cecaca6ad343baeca99673
ankitbhanja79 <38048930+ankitbhanja79@users.noreply.github.com>
2019-01-26 02:09:16 +0530

    guvi-b6.cpp

---
+++ guvi-b6.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+
+using namespace std;
+
+int main()
+{
+	int y;
+	cin>>y;
+	if(y%4==0)
+	{
+		cout<<"yes";
+	}
+	else
+	{
+		cout<<"not";
+	}
+	return 0;
+}
