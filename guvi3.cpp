3dccc51c9cacdf2c60728794698ca0764e3ebea1
ankitbhanja79 <38048930+ankitbhanja79@users.noreply.github.com>
2019-01-26 21:19:28 +0530

    guvi3.cpp

---
+++ guvi3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int n,a[100];
+	cin>>n;
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	for(int i=0;i<n;i++)
+	{
+		if(a[i]==i)
+		{
+			cout<<a[i]<<" ";
+		}
+	}
+	return 0;
+}
